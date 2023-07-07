fd437526bbfae899b551911b7a00356bcfd6f758
junlin77 <junlin.liow7@gmail.com>
2023-07-07 10:33:56 +0800

    Update email settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,9 +120,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'junlin.liow7@gmail.com'
-EMAIL_HOST_PASSWORD = 'hjchvemvisieldbh'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'junlin.liow7@gmail.com'
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
