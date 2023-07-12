8d43f2ff2f71d655386efb2609b0e5da89e9494c
junlin77 <junlin.liow7@gmail.com>
2023-07-12 10:30:45 +0800

    Update default search url

--- mysite/emailer/urls.py
+++ mysite/emailer/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.search, name="search"),
-    path('partial-search/', views.partial_search, name='partial_search'),
+    path("search/", views.search, name="search"),
+    path('', views.partial_search, name='partial_search'),
     path("send_to_kindle/", views.send_to_kindle, name="send_to_kindle"),
 ]
