f4ebe568832cf0a870a766977abaf010ac114aca
junlin77 <junlin.liow7@gmail.com>
2023-07-17 23:11:39 +0800

    Add allowed host

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-2c3kq6uuho!8ghnmxge99qgefth0z&(*0^(04rit5+(t2&ceo4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['libgenemailer.pythonanywhere.com',]
 
 
 # Application definition
