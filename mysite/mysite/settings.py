c162fc7b90ccadf2f21baa57df6b610d3bf06850
junlin77 <junlin.liow7@gmail.com>
2023-07-18 22:03:14 +0800

    Configure for deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-2c3kq6uuho!8ghnmxge99qgefth0z&(*0^(04rit5+(t2&ceo4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['libgenemailer.pythonanywhere.com',]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -114,6 +114,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
