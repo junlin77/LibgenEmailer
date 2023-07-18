12041e8b2561a243c27eafd0dbce31b2bc5a39f4
junlin77 <junlin.liow7@gmail.com>
2023-07-18 22:27:41 +0800

    Update requirements.txt

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-2c3kq6uuho!8ghnmxge99qgefth0z&(*0^(04rit5+(t2&ceo4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
